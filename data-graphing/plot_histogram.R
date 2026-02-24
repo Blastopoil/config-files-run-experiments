3128b003fca09dc852cfdfaaa44e4cf3a8a8fd2b
Felix <blastopoil@MaquinaPensante.blastopoil>
2026-02-24 01:51:06 +0100

    Improved massivly the script to allow three distinct types of barplots (via the 3 modes)

--- data-graphing/plot_histogram.R
+++ data-graphing/plot_histogram.R
@@ -4,6 +4,8 @@ suppressPackageStartupMessages({
   library(optparse)
   library(stringr)
   library(ggplot2)
+  library(ggthemes)
+  library(paletteer)
   library(dplyr)
   library(glue)
 })
@@ -19,17 +21,18 @@ option_list <- list(
   make_option(c("-m", "--metric"), type="character", default="IPC",
               help="Métrica (IPC, MPKI, CondMissRate) [default %default]", metavar="STR"),
   
-  make_option(c("-c", "--config"), type="character", default="ALL",
-              help="Config (ex: 'BigO3,SmallO3') [default %default]", metavar="LIST"),
-  
   make_option(c("-p", "--predictors"), type="character", default=NULL,
               help="Predictors (ex: 'TAGE_L,LocalBP') [default %default]", metavar="LIST"),
   
   make_option(c("-a", "--apps"), type="character", default=NULL,
-              help="Filtrar apps específicas (opcional incluso en batch) [default NULL]", metavar="LIST"),
+              help="Filters specific SPEC17 rate apps (none if not passed).\n\t\t'int' uses all SPEC17int apps\n\t\t'float' the SPEC17float apps\n\t\t'normal' a custom set of apps", metavar="LIST"),
   
-  make_option(c("-B", "--batch"), action="store_true", default=FALSE,
-              help="Si se activa, genera una gráfica individual por cada App encontrada automáticamente.")
+  make_option(c("-M", "--mode"), type="character", default="mean",
+              help="'mean': makes the mean of the data\n\t\t'separate': makes a single plot where each apps results gets its bar\n\t\t'batch': makes a plot for each apps result"),
+
+  make_option(c("-C", "--compare"), type="character", default="bp",
+              help="Attention! This argument is only used in 'separate' mode (not activated by default)\n\t\t'bp': groups bars by predictor\n\t\t'config': groups bars by core config\n\t\t'app': groups bars by app")
+            
 )
 
 opt <- parse_args(OptionParser(option_list=option_list))
@@ -60,36 +63,37 @@ if (!is.null(opt$predictors) && nzchar(opt$predictors)) {
   all_data <- all_data[all_data$cond_bp %in% predictors_filter, ]
 }
 
-# SPEC17 Integer
-# -a 500,502,505,520,523,525,531,541,548,557
-# SPEC17 Floating Point
-# -a 503,507,508,510,511,519,521,526,527,538,544,549,554
-# Normales
-# -a 507,508,510,519,521,526,544,548,549,557
-
 # Filters the SPEC17 apps
 apps_to_filter <- NULL
-if (!is.null(opt$apps) && nzchar(opt$apps)) {
-  apps_to_filter <- trimws(strsplit(opt$apps, ",")[[1]])
-  spec17_app_map <- c(
-    "500" = "500.perlbench_r", "502" = "502.gcc_r", "503" = "503.bwaves_r", "505" = "505.mcf_r", 
-    "507" = "507.cactuBSSN_r", "508" = "508.namd_r", "510" = "510.parest_r", "511" = "511.povray_r", 
-    "519" = "519.lbm_r", "520" = "520.omnetpp_r", "521" = "521.wrf_r", "523" = "523.xalancbmk_r", 
-    "525" = "525.x264_r", "526" = "526.blender_r", "527" = "527.cam4_r", "531" = "531.deepsjeng_r",
-    "538" = "538.imagick_r", "541" = "541.leela_r", "544" = "544.nab_r", "548" = "548.exchange2_r", 
-    "549" = "549.fotonik3d_r", "554" = "554.roms_r", "557" = "557.xz_r"
-  )
-  # Converts to full app name
-  apps_to_filter <- ifelse(
-    apps_to_filter %in% names(spec17_app_map),
-    unname(spec17_app_map[apps_to_filter]),
-    apps_to_filter
-  )
-  # Makes the actual filtering
-  all_data <- all_data[all_data$App %in% apps_to_filter, ]
+aux_apps <- opt$apps
+if (!is.null(opt$apps)) {
+  if (opt$apps == "int") {
+    aux_apps <- c("500,502,505,520,523,525,531,541,548,557")
+  } else if (opt$apps == "float") {
+    aux_apps <- c("503,507,508,510,511,519,521,526,527,538,544,549,554")
+  } else if (opt$apps == "normal") {
+    aux_apps <- c("507,508,510,519,521,526,544,548,549,557")
+  } 
+} else {
+  aux_apps <- c("500,502,503,505,507,508,510,511,519,520,521,523,525,526,527,531,538,541,544,548,549,554,557")
 }
-
-print(all_data)
+apps_to_filter <- trimws(strsplit(aux_apps, ",")[[1]])
+spec17_app_map <- c(
+  "500" = "500.perlbench_r", "502" = "502.gcc_r", "503" = "503.bwaves_r", "505" = "505.mcf_r", 
+  "507" = "507.cactuBSSN_r", "508" = "508.namd_r", "510" = "510.parest_r", "511" = "511.povray_r", 
+  "519" = "519.lbm_r", "520" = "520.omnetpp_r", "521" = "521.wrf_r", "523" = "523.xalancbmk_r", 
+  "525" = "525.x264_r", "526" = "526.blender_r", "527" = "527.cam4_r", "531" = "531.deepsjeng_r",
+  "538" = "538.imagick_r", "541" = "541.leela_r", "544" = "544.nab_r", "548" = "548.exchange2_r", 
+  "549" = "549.fotonik3d_r", "554" = "554.roms_r", "557" = "557.xz_r"
+)
+# Converts to full app name
+apps_to_filter <- ifelse(
+  apps_to_filter %in% names(spec17_app_map),
+  unname(spec17_app_map[apps_to_filter]),
+  apps_to_filter
+)
+# Makes the actual filtering
+all_data <- all_data[all_data$App %in% apps_to_filter, ]
 
 # Adds columns
 
@@ -102,63 +106,261 @@ if (!(opt$metric %in% colnames(all_data))) {
   stop(paste("Métrica no encontrada:", opt$metric))
 }
 
+print(all_data)
+
 # --- 4. Graph Creation ---
 
-# Creates one plot for all the apps or a set
-if (!opt$batch && is.null(apps_to_filter)) {
+# For the title of the plots' creation/s
+if (!is.null(opt$apps)) {
+  if (opt$apps == "int") {
+    apps_studied <- "(Integer) 500,502,505,520,523,525,531,541,548,557"
+  } else if (opt$apps == "float") {
+    apps_studied <- "(Floating Point) 503,507,508,510,511,519,521,526,527,538,544,549,554"
+  } else if (opt$apps == "normal") {
+    apps_studied <- "507,508,510,519,521,526,544,548,549,557"
+  } else {
+    apps_studied <- paste(apps_to_filter, collapse=", ")
+  }
+}
+
+# For the scale
+get_limit <- function(mode, limit) {
+  if (mode == "mean") {
+    limit
+  } else {
+    NULL
+  }
+}
+get_breaks <- function(mode, breaks) {
+  if (mode == "mean") {
+    breaks
+  } else {
+    NULL
+  }
+}
+get_scale <- function(mode) {
+  if (mode == "batch") {
+    NULL
+  } else if (opt$metric == "IPC") {
+    scale <- scale_y_continuous(# Líneas principales cada 0.5 unidades
+                      limits = get_limit(opt$mode, c(0, 4.5)),
+                      breaks = get_breaks(opt$mode, seq(0, 4.5, by = 0.5)), 
+                      # Líneas finas cada 0.1 unidades para lectura precisa
+                      minor_breaks = seq(0, 4, by = 0.1), 
+                      # Hace que las barras toquen el eje X (mult = c(abajo, arriba))
+                      expand = expansion(mult = c(0, 0.05)) 
+                      )
+    scale
+  } else if (opt$metric == "MPKI") {
+    scale <- scale_y_continuous(# Líneas principales cada 0.5 unidades
+                      limits = get_limit(opt$mode, c(-1, 90)),
+                      breaks = get_breaks(opt$mode, seq(0, 90, by = 10)),
+                      # Líneas finas cada 0.1 unidades para lectura precisa
+                      minor_breaks = seq(0, 4, by = 0.1), 
+                      # Hace que las barras toquen el eje X (mult = c(abajo, arriba))
+                      expand = expansion(mult = c(0, 0.05)) 
+                      )
+    scale
+  } else if (opt$metric == "CondMissRate") {
+    scale <- scale_y_continuous(# Líneas principales cada 0.5 unidades
+                      limits = get_limit(opt$mode, c(0, 35)),
+                      breaks = get_breaks(opt$mode, seq(0, 70, by = 5)), 
+                      # Líneas finas cada 0.1 unidades para lectura precisa
+                      minor_breaks = seq(0, 4, by = 0.1), 
+                      # Hace que las barras toquen el eje X (mult = c(abajo, arriba))
+                      expand = expansion(mult = c(0, 0.05)) 
+                      )
+    scale
+  }
+}
+
+# Creates one plot for all the apps or a set of apps
+if (opt$mode == "mean" && (length(apps_to_filter) > 1 || is.null(opt$apps))) {
+
+  # Label to filter by config and conditional branch predictor
+  all_data <- all_data %>%
+    mutate(group_label = paste(config, cond_bp, sep="\n"))
+  
+  # Calculates mean, sd, se and IC
+  my_sum <- all_data %>%
+  group_by(group_label, config, cond_bp) %>%
+  summarise( 
+      n=n(),
+      mean=mean(.data[[opt$metric]]),
+      sd=sd(.data[[opt$metric]]),
+      .groups = "drop"
+  ) %>%
+  mutate( se=sd/sqrt(n))  %>%
+  mutate( ic=se * qt((1-0.05)/2 + .5, n-1))
+
+  # The title and subtitle for the plot
+  if (is.null(opt$apps)) {
+    my_title = glue("Relation between {opt$metric} and the following configurations")
+    my_subtitle = NULL
+  } else {
+    my_title = glue("Relation between {opt$metric} and configurations using these SPEC17 apps:")
+    my_subtitle = glue("{apps_studied}")
+  }
+
+
+
+  # The actual figure creation
+  ggplot(my_sum, aes(x=group_label, y=mean, fill=config)) +
+  scale_fill_paletteer_d("nationalparkcolors::Arches") +
+  geom_bar(stat="identity", position=position_dodge(width=0.8), 
+           width=0.7, 
+           # Contorno negro para definir la barra
+           color = "black",
+           # Grosor del contorno
+           linewidth = 0.3,
+           # Un poco de transparencia para suavizar el tono)
+           #alpha = 0.85
+          ) +
+  geom_errorbar(aes(ymin=mean-ic, ymax=mean+ic), width=0.2, colour="black", alpha=0.9, linewidth=0.4, position=position_dodge(0.7)) +
+  labs(title=my_title, subtitle=my_subtitle, y=opt$metric, x="Core + Branch Predictor") +
+  theme_bw() + 
+  theme(text = element_text(family = "sans", size = 18),
+        # Rejilla principal muy tenue
+        panel.grid.major = element_line(color = "grey90"),
+        # Elimina rejilla secundaria
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(size=18, face="bold", margin=margin(b=10), hjust=0.5),
+        plot.subtitle = element_text(size=16, face="bold", hjust=0.5),
+        #axis.text.x  = element_text(size=14, angle=45, hjust=1),
+        axis.title.x = element_text(margin = margin(t = 10)),  # separa el título de las etiquetas
+        # Mover la leyenda arriba
+        legend.position = "top"
+       ) +
+  get_scale(opt$mode)
+  
+  
+  ggsave(opt$output, width=10, height=6)
+  #system(paste("xdg-open", opt$output))
+
+
+} else if (opt$mode == "separate" && (length(apps_to_filter) > 1 || is.null(opt$apps))) {
+
+  # Determina qué usar para agrupar visualmente según --compare
+  if (opt$compare == "bp") {
+    # Agrupa por predictor: X = config+app, fill = bp
+    all_data <- all_data %>%
+      mutate(group_label = paste(config, App, sep="\n"))
+    fill_var <- "cond_bp"
+  } else if (opt$compare == "config") {
+    # Agrupa por config: X = bp+app, fill = config
+    all_data <- all_data %>%
+      mutate(group_label = paste(cond_bp, App, sep="\n"))
+    fill_var <- "config"
+  } else if (opt$compare == "app") {
+    # Agrupa por app: X = config+bp, fill = app
+    all_data <- all_data %>%
+      mutate(group_label = paste(config, cond_bp, sep="\n"))
+    fill_var <- "App"
+  } else {
+    stop("Invalid --compare option. Use 'bp', 'config', or 'app'")
+  }
+
+  # Calcula media manteniendo todas las dimensiones
+  my_sum <- all_data %>%
+  group_by(group_label, config, cond_bp, App) %>%
+  summarise( 
+      n=n(),
+      mean=mean(.data[[opt$metric]]),
+      .groups = "drop"
+  )
+
+  # The title for the plot
+  apps_studied <- paste(apps_to_filter, collapse=", ")
+  my_title = glue("Relation between {opt$metric} and configurations using these SPEC17 apps: {apps_studied}\nFocuses on comparing {opt$compare}")
+
+  # The actual figure creation (usa fill_var dinámicamente)
+  ggplot(my_sum, aes(x=group_label, y=mean, fill=.data[[fill_var]])) +
+  scale_fill_paletteer_d("nationalparkcolors::Arches") +
+  geom_bar(stat="identity", position=position_dodge(width=0.8), 
+           width=0.7, 
+           # Contorno negro para definir la barra
+           color = "black",
+           # Grosor del contorno
+           linewidth = 0.3,
+           # Un poco de transparencia para suavizar el tono)
+           #alpha = 0.85
+          ) +
+  labs(title=my_title, y=opt$metric, x="Grouping", fill=fill_var) +
+  theme_bw() + 
+  theme(text = element_text(family = "sans", size=18),
+        # Rejilla principal muy tenue
+        panel.grid.major = element_line(color = "grey90"),
+        # Elimina rejilla secundaria
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(size=14, face="bold", margin=margin(b=10), hjust=0.5),
+        axis.text.x = element_text(angle=45, hjust=1),
+        # Mover la leyenda arriba
+        legend.position = "top"
+       ) +
+  get_scale(opt$mode)
+
+
+  ggsave(opt$output, width=14, height=7)
+  system(paste("xdg-open", opt$output))
+
+
+} else if (opt$mode == "batch" || length(apps_to_filter) == 1) {
+  
+  # Creates one plot per requested app
+
+  for (app in apps_to_filter) {
+    # Filters only this app
+    app_data <- all_data[all_data$App == app, ]
+    
+    if (nrow(app_data) == 0) next
 
     # Label to filter by config and conditional branch predictor
-    all_data <- all_data %>%
-        mutate(group_label = paste(config, cond_bp, sep="\n"))
-    
-    # Calculates mean, sd, se and IC
-    my_sum <- all_data %>%
+    app_data <- app_data %>%
+      mutate(group_label = paste(config, cond_bp, sep="\n"))
+
+    # Calculates mean
+    my_sum <- app_data %>%
     group_by(group_label, config, cond_bp) %>%
     summarise( 
         n=n(),
         mean=mean(.data[[opt$metric]]),
-        sd=sd(.data[[opt$metric]]),
         .groups = "drop"
-    ) %>%
-    mutate( se=sd/sqrt(n))  %>%
-    mutate( ic=se * qt((1-0.05)/2 + .5, n-1))
+    )
 
-    # Confidence interval
+    # The actual figure creation
     ggplot(my_sum, aes(x=group_label, y=mean, fill=config)) +
-    geom_bar(stat="identity", position=position_dodge(), width=0.7, alpha=0.9) +
-    geom_errorbar(aes(ymin=mean-ic, ymax=mean+ic), width=0.4, colour="orange", alpha=0.9, linewidth=1.5, position=position_dodge(0.7)) +
-    labs(title=glue("Relation between {opt$metric} and the following configurations"), y=opt$metric, x="Core + Branch Predictor")
+    scale_fill_paletteer_d("beyonce::X72") +
+    geom_bar(stat="identity", position=position_dodge(width=0.8), 
+            width=0.7, 
+            # Contorno negro para definir la barra
+            color = "black",
+            # Grosor del contorno
+            linewidth = 0.3,
+            # Un poco de transparencia para suavizar el tono)
+            #alpha = 0.85
+            ) +
+    labs(title=glue("Relation between {opt$metric} and configurations using the SPEC17 app {app}"), y=opt$metric, x="Core + Branch Predictor") +
+    theme_bw() + 
+    theme(text = element_text(family = "sans", size = 18),
+          # Rejilla principal muy tenue
+          panel.grid.major = element_line(color = "grey90"),
+          # Elimina rejilla secundaria
+          panel.grid.minor = element_blank(),
+          plot.title = element_text(size=14, face="bold", margin=margin(b=10), hjust=0.5),
+          #axis.text.x  = element_text(size=14, angle=45, hjust=1),
+          axis.title.x = element_text(margin = margin(t = 10)),  # separa el título de las etiquetas
+          # Mover la leyenda arriba
+          legend.position = "top"
+        ) +
+    get_scale(opt$mode)
     
-    ggsave(opt$output, width=10, height=6)
+    # Generates a unique file for each app
+    app_num <- str_extract(app, "^[0-9]+")
+    output_file <- str_replace(opt$output, "\\.png$", paste0("_", app_num, ".png"))
+    ggsave(output_file, width=10, height=6)
+  }
+
+
+} else {
+  print("Something went wrong with the mode and/or the app selection, no graphs generated")
 }
-# Creates one plot for a set of apps
-if (!opt$batch && !is.null(apps_to_filter)) {
-
-    # Label to filter by config, conditional branch predictor and apps
-    all_data <- all_data %>%
-        mutate(group_label = paste(config, cond_bp, sep="\n"))
-    
-    # Calculates mean, sd, se and IC
-    my_sum <- all_data %>%
-    group_by(group_label, config, cond_bp) %>%
-    summarise( 
-        n=n(),
-        mean=mean(.data[[opt$metric]]),
-        sd=sd(.data[[opt$metric]]),
-        .groups = "drop"
-    ) %>%
-    mutate( se=sd/sqrt(n))  %>%
-    mutate( ic=se * qt((1-0.05)/2 + .5, n-1))
-
-    apps_studied <- paste(apps_to_filter, collapse=", ")
-    # Confidence interval
-    ggplot(my_sum, aes(x=group_label, y=mean, fill=config)) +
-    geom_bar(stat="identity", position=position_dodge(), width=0.7, alpha=0.9) +
-    geom_errorbar(aes(ymin=mean-ic, ymax=mean+ic), width=0.4, colour="orange", alpha=0.9, linewidth=1.5, position=position_dodge(0.7)) +
-    labs(title=glue("Relation between {opt$metric} and configurations using these SPEC17 apps: {apps_studied}"), y=opt$metric, x="Core + Branch Predictor") +
-    theme(plot.title = element_text(size=10, face="bold", margin=margin(b=10), hjust=0.5),
-          axis.text.x = element_text(angle=45, hjust=1))
-    
-    ggsave(opt$output, width=10, height=6)
-}
-
